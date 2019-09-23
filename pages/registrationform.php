441320edd76b1e0436d11470721f137528f4809f
Jesdin Raphael <jesdinraphael@gmail.com>
2019-09-23 06:56:22 +0530

    register User

--- pages/registrationform.php
+++ pages/registrationform.php
@@ -6,14 +6,14 @@
     </head>
     <body background="..\images\background.jpg">
         <div class=div>
-            <form class=form>
+            <form class=form method="POST" action="register.php">
                 <p class=p1>REGISTER</p>
-                <input id="fName" type="text" placeholder="Enter first name">
-                <input id="lName" type="text" placeholder="Enter last name">
-                <input id="username" type="text" placeholder="Enter username">
-                <input id="emailId" type="text" placeholder="Enter email id">
-                <input id="password" type="password" placeholder="Enter password">
-                <input id="cPassword" type="password" placeholder="Confirm password">
+                <input id="fName" name="fName" type="text" placeholder="Enter first name" required minlength="2" maxlength="15">
+                <input id="lName" name="lName" type="text" placeholder="Enter last name" required minlength="2" maxlength="15">
+                <input id="username" name="uName" type="text" placeholder="Enter username" required minlength="4" maxlength="15">
+                <input id="emailId" name="email" type="email" placeholder="Enter email id" required>
+                <input id="password" name="pss" type="password" placeholder="Enter password" required>
+                <input id="cPassword" name="cpss" type="password" placeholder="Confirm password" required>
                 <button id="done" type="submit">Register</button>
             </form>
         </div>
